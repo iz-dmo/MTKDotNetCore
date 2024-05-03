2ec11b6ceebfe9562b69ab612dc6a3f1eeb783b8
iz-dmo <mtk22718@gmail.com>
2024-05-03 16:44:55 +0630

    DapperService

---
+++ MTKDotNetCore.Shared/DapperService.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+namespace MTKDotNetCore.Shared;
+
+public class DapperService
+{
+    private readonly string _connectionString;
+    public DapperService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public List<M> Query<M>(string query, object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        // if(param != null){
+        //     var lst = db.Query<M>(query,param).ToList();
+
+        // }else{
+        //     var lst = db.Query<M>(query ).ToList();
+
+        // }
+        var lst = db.Query<M>(query,param).ToList();
+        return lst;
+    }
+
+    public M QueryFirstOrDefault<M>(string query, object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        // if(param != null){
+        //     var lst = db.Query<M>(query,param).ToList();
+
+        // }else{
+        //     var lst = db.Query<M>(query ).ToList();
+
+        // }
+        var item = db.Query<M>(query,param).FirstOrDefault();
+        return item!;
+    }
+
+    public int Execute(string query, object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        return db.Execute(query,param);
+
+    }
+}
